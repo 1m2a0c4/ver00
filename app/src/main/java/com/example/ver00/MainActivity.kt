ba9ebb99287062f22cd81dbb50874f7b632d52ee
1m2c0a4 <macaroni1204@gmail.com>
2020-01-17 12:38:07 +0900

    2020/01/17

--- app/src/main/java/com/example/ver00/MainActivity.kt
+++ app/src/main/java/com/example/ver00/MainActivity.kt
@@ -17,17 +17,40 @@ class MainActivity : AppCompatActivity() {
 
         val buttonUp = findViewById<Button>(R.id.up) as Button
         val buttonDown = findViewById<Button>(R.id.down) as Button
+        val buttonRight = findViewById<Button>(R.id.down) as Button
+        val buttonLeft = findViewById<Button>(R.id.down) as Button
+
+
 
 
         buttonDown.setOnClickListener() {
             flag = "down"
+            val messageView: TextView = findViewById(R.id.textV)
+            messageView.text = flag
         }
 
         buttonUp.setOnClickListener() {
             flag = "up"
+            val messageView: TextView = findViewById(R.id.textV)
+            messageView.text = flag
         }
 
+        buttonRight.setOnClickListener() {
+            flag = "right"
+            val messageView: TextView = findViewById(R.id.textV)
+            messageView.text = flag
+        }
+
+        buttonLeft.setOnClickListener() {
+            flag = "left"
+            val messageView: TextView = findViewById(R.id.textV)
+            messageView.text = flag
+        }
+
+
+       /*
         val messageView: TextView = findViewById(R.id.textV)
+
         messageView.text =
             when (flag) {
                 "up" ->
@@ -39,8 +62,11 @@ class MainActivity : AppCompatActivity() {
                 else ->
                     "無効な操作"
             }
+
+        */
     }
 }
+
 
 
 
